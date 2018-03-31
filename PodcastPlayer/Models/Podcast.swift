88f8ee6d0b9f1343fde5ffc0034bccff7919e533
Magnus Holm <mail@magnusholm.com>
2018-03-31 15:12:48 +0200

    Add dummyPodcasts to array and populate tableviewCell with info

---
+++ PodcastPlayer/Models/Podcast.swift
@@ -0,0 +1,15 @@
+//
+//  Podcast.swift
+//  PodcastPlayer
+//
+//  Created by magnus holm on 31/03/2018.
+//  Copyright © 2018 magnus holm. All rights reserved.
+//
+
+import Foundation
+
+struct Podcast {
+    let name: String
+    let artistName: String
+}
+
--- PodcastPlayer/ViewControllers/PodcastsSearchViewController.swift
+++ PodcastPlayer/ViewControllers/PodcastsSearchViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class PodcastsSearchViewController: UITableViewController {
     
+    let podcasts = [
+        Podcast(name: "A podcast", artistName: "Magnus"),
+        Podcast(name: "Another podcast", artistName: "Magnus1"),
+        Podcast(name: "A third podcast", artistName: "Magnus2")
+    ]
+    
+    let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = podcasts[indexPath.row].name
+        return cell
     }
     
 }
