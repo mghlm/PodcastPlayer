817ab1d546312af2bc920b042424f1ab1cd348eb
Magnus Holm <mail@magnusholm.com>
2018-03-31 14:57:11 +0200

    Refactor, add comments

--- PodcastPlayer/MainTabBarController.swift
+++ PodcastPlayer/MainTabBarController.swift
@@ -12,8 +12,15 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UINavigationBar.appearance().prefersLargeTitles = true
         tabBar.tintColor = .purple
         
+        setupViewControllers()
+    }
+    
+    // MARK: - Setup Functions
+    
+    fileprivate func setupViewControllers() {
         let favoritesNavigationController = generateNavigationController(with: ViewController(), title: "Favorites", image: #imageLiteral(resourceName: "favorites"))
         let searchNavigationController = generateNavigationController(with: ViewController(), title: "Search", image: #imageLiteral(resourceName: "search"))
         let downloadsNavigationController = generateNavigationController(with: ViewController(), title: "Downloads", image: #imageLiteral(resourceName: "downloads"))
@@ -21,17 +28,15 @@ class MainTabBarController: UITabBarController {
         viewControllers = [favoritesNavigationController, searchNavigationController, downloadsNavigationController]
     }
     
+    // MARK: - Helper functions
+    
     fileprivate func generateNavigationController(with rootViewController: UIViewController, title: String, image: UIImage) -> UINavigationController {
         
         let navigationController = UINavigationController(rootViewController: rootViewController)
-        navigationController.navigationBar.prefersLargeTitles = true 
         rootViewController.navigationItem.title = title
         navigationController.tabBarItem.title = title
         navigationController.tabBarItem.image = image
         
         return navigationController
-        
     }
-    
-    
 }
